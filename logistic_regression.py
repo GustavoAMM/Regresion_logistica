dac4d824a4cda79d61d502c6992c7e8de32c90fc
Angel Montoya <j.gustavo.m245@gmail.com>
2023-03-17 06:04:57 -0600

    Regresion logistica

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,9 +2,17 @@ import numpy as np
 import pandas as pd
 
 class LogisticRegression():
+
+    #Inicializar la clase. 
+    #En este caso, no se hace nada, por lo que el método está vacío.
     def __init__(self) -> None:
         pass
-
+    
+    #Entrenar el modelo. 
+    #El método utiliza el algoritmo de gradiente descendente para ajustar los valores de los coeficientes de theta a los datos de entrenamiento. 
+    #Devuelve dos listas: 
+    #   los valores de pérdida de cada iteración
+    #   Número de iteración correspondiente.
     def fit(self, X, y, learning_rate=0.0001, epochs=1000, bias=True):
         n = int(len(X))  # numero de elementos de x
         y = np.resize(y, (n, 1))
@@ -31,9 +39,11 @@ class LogisticRegression():
         print(thetas)
         return (iter_, errores)
 
+    #Calcula la hipótesis del modelo, que es la función sigmoidal.
     def h(self, z):
         return 1 / (1 + np.exp(-z))
 
+    #Calcula la pérdida del modelo utilizando la función de pérdida de la regresión logística
     def mean_squared_error(self, y, h):
         n = len(y)
         loss = (-1 / n) * np.sum(y * np.log(h) + (1 - y) * np.log(1 - h))
